2d700b749dcbb02c9d261e0f56c5403cdfeed3b2
baegteun <baegteun@gmail.com>
2022-05-22 21:04:16 +0900

    Initial Commit

---
+++ Shared/AppStoreHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreHeroApp.swift
+//  Shared
+//
+//  Created by 최형우 on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
